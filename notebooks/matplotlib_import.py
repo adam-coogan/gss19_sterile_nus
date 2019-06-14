10940abac6fe8c71dc826b688f480c91b498c881
Kosio <kosiokarchev@gmail.com>
2019-06-14 03:23:30 +0200

    pen and paper (and plots)

---
+++ notebooks/matplotlib_import.py
@@ -0,0 +1,6 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.size'] = plt.rcParams['axes.labelsize'] = plt.rcParams['xtick.labelsize'] = plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+plt.rcParams['lines.linewidth'] = 2
